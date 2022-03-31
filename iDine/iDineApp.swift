5e72a8557de9f95ca08dcce23be02723db4f5ab0
Emrah <emrahkbulut@gmail.com>
2022-04-01 00:24:33 +0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Emrah Karabulut on 1.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
